84d0a58f5d41f348f7c4e191378df850ecf2f32f
hilmyshabir <hilmyshabir10@gmail.com>
2021-06-16 00:01:59 +0700

    nampilin nama customer

--- Cinematix/application/views/templates/header_customer.php
+++ Cinematix/application/views/templates/header_customer.php
@@ -49,6 +49,26 @@
                     <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-2 rounded" href="#theaters">Theaters</a></li>
                     <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-2 rounded" href="#about">About</a></li>
                     <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-2 rounded" href="#history">History</a></li>
+
+                    <li class="nav-item mx-0 mx-lg-1">
+                        <a class="nav-link dropdown-toggle py-3 px-0 px-lg-2 rounded" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <?php foreach ($pemesanan as $pe) :  ?>
+                                <span class="mr-2 d-none d-lg-inline text-grey-600"><?= $pe['User']; ?></span>
+                            <?php endforeach; ?>
+                            <!-- <img class="img-profile rounded-circle" src="<?= base_url(); ?>assets/img/undraw_profile.svg" width=20 height=20> -->
+                            <img src="<?= base_url(); ?>assets/img/undraw_profile.svg" width=25 height=25>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                            <a class="dropdown-item" href="#">
+                                <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                My Profile
+                            </a>
+                            <a class="dropdown-item" href="<?= base_url('Auth/logout'); ?>">
+                                <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                Logout
+                            </a>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </div>
